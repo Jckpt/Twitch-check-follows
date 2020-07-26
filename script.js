d50ff3b0fe7c9f71c2e3e79aad580f516f46d2e6
Jckpt <64931967+Jckpt@users.noreply.github.com>
2020-07-26 22:39:23 +0200

    changed from .then to async

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ let all_nicks = [];
 let count = 0;
 let nick_json = '';
 // fetchuje liste uzytkownikow aktualnie zalogowanych na czacie (fetches users that are currently connected to chat)
-let activate = () => {
+let activate = async () => {
   $('#informator').html('<img src="https://cdn.frankerfacez.com/emoticon/149346/2"> praca praca..');
   let channel_name = document.getElementById('chat').value.toLowerCase();
   let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
     targetUrl = `https://tmi.twitch.tv/group/user/${channel_name}/chatters`;
-  fetch(proxyUrl + targetUrl)
-    .then((blob) => blob.json())
-    .then(({ chatters: { viewers }, chatters: { vips }, chatters: { moderators } }) => {
-      const everyViewer = [...viewers, ...vips, ...moderators];
-      for (let j = 0; j < everyViewer.length; j++) {
-        findID(everyViewer[j].toLowerCase());
-      }
-    });
+  const response = await fetch(proxyUrl + targetUrl);
+  const blob = await response.json();
+  let {
+    chatters: { viewers, vips, moderators },
+  } = blob;
+  const everyViewer = [...viewers, ...vips, ...moderators];
+  for (let j = 0; j < everyViewer.length; j++) {
+    findID(everyViewer[j].toLowerCase());
+  }
 };
 
 let calculate_date = (follow_date) => {
@@ -55,54 +56,54 @@ document.querySelectorAll('input').forEach((item) => {
   });
 });
 // szuka id uzytkownika po nicku (searches user ID by their nick)
-let findID = (name) => {
-  fetch(`https://api.twitch.tv/kraken/users?login=${name}`, {
+let findID = async (name) => {
+  let response = await fetch(`https://api.twitch.tv/kraken/users?login=${name}`, {
     headers: {
       Accept: 'application/vnd.twitchtv.v5+json',
       'Client-ID': 'k1c1q8lb5qd9oxn9cnfjnh2manhuo0',
     },
-  })
-    .then((response) => response.json())
-    .then(({ users: [user] }) => {
-      if (!$('#checkbox_json').prop('checked')) {
-        insertChannelHTML(user._id, name, user.logo);
-      } else {
-        insertChannelHTML(user._id, name, null);
-      }
-    });
+  });
+  let data = await response.json();
+  let {
+    users: [user],
+  } = data;
+  if (!$('#checkbox_json').prop('checked')) {
+    insertChannelHTML(user._id, name, user.logo);
+  } else {
+    insertChannelHTML(user._id, name, null);
+  }
 };
 
-let insertChannelHTML = (id, user_nick, avatar) => {
+let insertChannelHTML = async (id, user_nick, avatar) => {
   let wanted_channel = document.getElementById('wanted_channel').value.toLowerCase();
   let raw_json;
   if (!$('#checkbox_json').prop('checked')) {
     avatar = avatar.replace(/300x300/, '70x70'); // zmiana wielkosci awataru na mniejszy (changes size of avatars in URL)
   }
-  fetch('https://api.twitch.tv/kraken/users/' + id + '/follows/channels', {
+  let response = await fetch('https://api.twitch.tv/kraken/users/' + id + '/follows/channels', {
     headers: {
       Accept: 'application/vnd.twitchtv.v5+json',
       'Client-ID': 'k1c1q8lb5qd9oxn9cnfjnh2manhuo0',
     },
-  })
-    .then((response) => response.json())
-    .then(({ follows }) => {
-      // result zawiera followy uzytkownika (result contains follows of user)
-      for (let i = 0; i < follows.length; i++) {
-        if (follows[i].channel.name === wanted_channel) {
-          days = calculate_date(follows[i].created_at);
-          if (!$('#checkbox_json').prop('checked')) {
-            document.getElementById(
-              'app_output'
-            ).innerHTML += `<div class="user-info"><div><img class="avatar-image" src="${avatar}" title="${days} dni"></div><div class="asd">${user_nick}</div></div>`;
-          } else {
-            raw_json = createJSON(user_nick);
-            document.getElementById('app_output').innerHTML = `<div id="json_output">${raw_json}</div>`;
-          }
-          $('#informator').html('');
-          break;
-        }
+  });
+  let data = await response.json();
+  let { follows } = data;
+  // result zawiera followy uzytkownika (result contains follows of user)
+  for (let i = 0; i < follows.length; i++) {
+    if (follows[i].channel.name === wanted_channel) {
+      days = calculate_date(follows[i].created_at);
+      if (!$('#checkbox_json').prop('checked')) {
+        document.getElementById(
+          'app_output'
+        ).innerHTML += `<div class="user-info"><div><img class="avatar-image" src="${avatar}" title="${days} dni"></div><div class="asd">${user_nick}</div></div>`;
+      } else {
+        raw_json = createJSON(user_nick);
+        document.getElementById('app_output').innerHTML = `<div id="json_output">${raw_json}</div>`;
       }
-    });
+      $('#informator').html('');
+      break;
+    }
+  }
   $('#informator').html(
     '<img src="https://cdn.betterttv.net/emote/56c2cff2d9ec6bf744247bf1/2x"><img src="https://cdn.betterttv.net/emote/576befd71f520d6039622f7e/2x"> przetwarzanie danych'
   );
