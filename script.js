412b4c84a3c852d9cf129d7097dc5c68bfd1d0fe
Jckpt <64931967+Jckpt@users.noreply.github.com>
2020-07-29 19:26:15 +0200

    no jquery and json fix

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let all_nicks = [];
-let count = 0;
-let nick_json = '';
+let nickJson = '';
 // fetchuje liste uzytkownikow aktualnie zalogowanych na czacie (fetches users that are currently connected to chat)
 let activate = async () => {
-  $('#informator').html('<img src="https://cdn.frankerfacez.com/emoticon/149346/2"> praca praca..');
-  let channel_name = document.getElementById('chat').value.toLowerCase();
+  document.querySelector('#informator').innerHTML = '<img src="https://cdn.frankerfacez.com/emoticon/149346/2"> praca praca..';
+  let channelName = document.getElementById('chat').value.toLowerCase();
   let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    targetUrl = `https://tmi.twitch.tv/group/user/${channel_name}/chatters`;
+    targetUrl = `https://tmi.twitch.tv/group/user/${channelName}/chatters`;
   const response = await fetch(proxyUrl + targetUrl);
   const blob = await response.json();
   let {
@@ -18,35 +16,40 @@ let activate = async () => {
   }
 };
 
-let calculate_date = (follow_date) => {
-  follow_date = follow_date.substring(0, 10);
-  follow_date = new Date(follow_date);
+let calculate_date = (followDate) => {
+  followDate = followDate.substring(0, 10);
+  followDate = new Date(followDate);
   let d = new Date();
-  let diff = new Date(d - Date.parse(follow_date)) / 86400000;
+  let diff = new Date(d - Date.parse(followDate)) / 86400000;
   diff = Math.trunc(diff);
   return diff;
 };
 
-let createJSON = (user_nick) => {
-  let raw_json;
-  all_nicks.push(user_nick);
-  nick_json += '{"nick":"' + all_nicks[count] + '"},';
-  raw_json = '{"users":[' + nick_json + ']}';
-  raw_json = raw_json.replace(/},]/, '}]');
-  count++;
-  return raw_json;
+let createJSON = (userNick, reset) => {
+  let rawJson;
+  if (reset) {
+    rawJson = '';
+    nickJson = '';
+  }
+  nickJson += `{"nick":"${userNick}"},`;
+  rawJson = `{"users":[${nickJson}]}`;
+  rawJson = rawJson.replace(/},]/, '}]');
+  return rawJson;
 };
 document.querySelectorAll('input').forEach((item) => {
   item.addEventListener('keyup', (event) => {
     if (event.which == 13) {
       if (document.querySelectorAll('input')[0].value == '' || document.querySelectorAll('input')[1].value == '') {
-        $('#informator').html('<img src="https://cdn.frankerfacez.com/emoticon/149346/2"> jedno z pól jest puste');
+        document.querySelector('#informator').innerHTML = 'jedno z pól jest puste';
       } else {
-        if ($('.user-info')[0]) {
-          $('div.user-info').remove();
+        if (document.body.contains(document.getElementsByClassName('user-info')[0])) {
+          document.querySelectorAll('.user-info').forEach(function (a) {
+            a.remove();
+          });
           activate();
-        } else if ($('#json_output')) {
-          $('#json_output').remove();
+        } else if (document.body.contains(document.getElementById('json_output'))) {
+          document.querySelector('#json_output').remove();
+          createJSON(null, true);
           activate();
         } else {
           activate();
@@ -67,17 +70,16 @@ let findID = async (name) => {
   let {
     users: [user],
   } = data;
-  if (!$('#checkbox_json').prop('checked')) {
+  if (!document.querySelector('#checkbox_json').checked) {
     insertChannelHTML(user._id, name, user.logo);
   } else {
     insertChannelHTML(user._id, name, null);
   }
 };
 
-let insertChannelHTML = async (id, user_nick, avatar) => {
-  let wanted_channel = document.getElementById('wanted_channel').value.toLowerCase();
-  let raw_json;
-  if (!$('#checkbox_json').prop('checked')) {
+let insertChannelHTML = async (id, userNick, avatar) => {
+  let wantedChannel = document.getElementById('wanted_channel').value.toLowerCase();
+  if (!document.querySelector('#checkbox_json').checked) {
     avatar = avatar.replace(/300x300/, '70x70'); // zmiana wielkosci awataru na mniejszy (changes size of avatars in URL)
   }
   let response = await fetch('https://api.twitch.tv/kraken/users/' + id + '/follows/channels', {
@@ -88,23 +90,20 @@ let insertChannelHTML = async (id, user_nick, avatar) => {
   });
   let data = await response.json();
   let { follows } = data;
-  // result zawiera followy uzytkownika (result contains follows of user)
+  document.querySelector('#informator').innerHTML = '';
   for (let i = 0; i < follows.length; i++) {
-    if (follows[i].channel.name === wanted_channel) {
-      days = calculate_date(follows[i].created_at);
-      if (!$('#checkbox_json').prop('checked')) {
+    if (follows[i].channel.name === wantedChannel) {
+      let days = calculate_date(follows[i].created_at);
+      if (!document.querySelector('#checkbox_json').checked) {
         document.getElementById(
           'app_output'
-        ).innerHTML += `<div class="user-info"><div><img class="avatar-image" src="${avatar}" title="${days} dni"></div><div class="asd">${user_nick}</div></div>`;
+        ).innerHTML += `<div class="user-info"><div><img class="avatar-image" src="${avatar}" title="${days} dni"></div><div class="asd">${userNick}</div></div>`;
       } else {
-        raw_json = createJSON(user_nick);
-        document.getElementById('app_output').innerHTML = `<div id="json_output">${raw_json}</div>`;
+        let rawJson = createJSON(userNick);
+        document.getElementById('app_output').innerHTML = `<div id="json_output">${rawJson}</div>`;
+        rawJson = '';
       }
-      $('#informator').html('');
       break;
     }
   }
-  $('#informator').html(
-    '<img src="https://cdn.betterttv.net/emote/56c2cff2d9ec6bf744247bf1/2x"><img src="https://cdn.betterttv.net/emote/576befd71f520d6039622f7e/2x"> przetwarzanie danych'
-  );
 };
